9c0cd8a0575d4f2680509df45aa4e634933bcb1b
lukehayes <lukepurplehayes@gmail.com>
2024-09-03 16:05:36 +0100

    Added hashed version of password

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -15,7 +15,7 @@ class AppFixtures extends Fixture
 
         $user = new User();
         $user->setUsername("developer");
-        $user->setPassword("password");
+        $user->setPassword('$2y$13$zyE/rhbg09arrBs7gCJ9Weok/RhBc1rsyaLBrpyR/WTEUD4nFNcV6');
 
         $manager->persist($user);
 
