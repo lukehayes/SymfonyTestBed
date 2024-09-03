689e902614fd8b231050cd54b1018b8bd4d812a6
lukehayes <lukepurplehayes@gmail.com>
2024-09-03 09:42:52 +0100

    Started implementation on RegistrationController

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasher;
+use App\Entity\User;
 
 class RegistrationController extends AbstractController
 {
@@ -17,21 +19,24 @@ class RegistrationController extends AbstractController
         ]);
     }
 
-    // public function index(UserPasswordHasherInterface $passwordHasher): Response
-    // {
-    //     // TODO Copied from Symfony docs - learn and implement security.
-    //
-    //     // ... e.g. get the user data from a registration form
-    //     $user = new User(...);
-    //     $plaintextPassword = ...;
-    //
-    //     // hash the password (based on the security.yaml config for the $user class)
-    //     $hashedPassword = $passwordHasher->hashPassword(
-    //         $user,
-    //         $plaintextPassword
-    //     );
-    //     $user->setPassword($hashedPassword);
-    //
-    //     // ...
-    // }
+    #[Route('/register_user', name: 'user_registration')]
+    public function registerUser(UserPasswordHasherInterface $passwordHasher): Response
+    {
+        // TODO Copied from Symfony docs - learn and implement security.
+        $plaintextPassword = "password";
+
+        // ... e.g. get the user data from a registration form
+        $user = new User();
+        $user->setUsername("developer");
+
+        // hash the password (based on the security.yaml config for the $user class)
+        $hashedPassword = $passwordHasher->hashPassword(
+            $user,
+            $plaintextPassword
+        );
+
+        dump($hashedPassword);
+
+        $user->setPassword($hashedPassword);
+    }
 }
