d58669596f052ce6150b19aef5524b16b9e58df8
lukehayes <lukepurplehayes@gmail.com>
2024-09-03 09:42:10 +0100

    Added User class to AppFixture

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\User;
 
 class AppFixtures extends Fixture
 {
@@ -11,6 +12,12 @@ class AppFixtures extends Fixture
     {
         // $product = new Product();
         // $manager->persist($product);
+
+        $user = new User();
+        $user->setUsername("developer");
+        $user->setPassword("password");
+
+        $manager->persist($user);
 
         $manager->flush();
     }
